62962e97c4e5466e4905f838d69bdd88c92880e7
Thiago Frederico <thiagofrederico3@gmail.com>
2020-10-21 10:51:28 -0300

    criando tabelas para o bd e adicionando relacionamento de tabelas

--- backend/db_techfacil.sql
+++ backend/db_techfacil.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 19-Out-2020 às 22:55
--- Versão do servidor: 5.7.31
--- versão do PHP: 7.3.21
+-- Host: 127.0.0.1:3308
+-- Tempo de geração: 21-Out-2020 às 13:46
+-- Versão do servidor: 8.0.18
+-- versão do PHP: 7.4.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,19 +31,86 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `tb_administradores`;
 CREATE TABLE IF NOT EXISTS `tb_administradores` (
   `idAdm` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) COLLATE utf8_bin NOT NULL,
-  `email` varchar(50) COLLATE utf8_bin NOT NULL,
-  `senha` varchar(40) COLLATE utf8_bin NOT NULL,
-  `cpf` varchar(15) COLLATE utf8_bin NOT NULL,
+  `nome` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `senha` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `cpf` varchar(15) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`idAdm`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Tabela para controle de acesso de administradores.';
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Tabela para controle de acesso de administradores.';
 
 --
 -- Extraindo dados da tabela `tb_administradores`
 --
 
 INSERT INTO `tb_administradores` (`idAdm`, `nome`, `email`, `senha`, `cpf`) VALUES
-(1, 'Vitor Pereira', 'vitor.per55@gmail.com', '29515bb9a5d5e558e2b3ba71e3b6e037', '50576009822');
+(1, 'Vitor Pereira', 'vitor.per55@gmail.com', '29515bb9a5d5e558e2b3ba71e3b6e037', '50576009822'),
+(2, 'Thiago Frederico', 'thiagofrederico3@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', '56484961237');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_contato`
+--
+
+DROP TABLE IF EXISTS `tb_contato`;
+CREATE TABLE IF NOT EXISTS `tb_contato` (
+  `idContato` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
+  `telefone` bigint(20) NOT NULL,
+  `emailSecundario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idContato`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_endereco`
+--
+
+DROP TABLE IF EXISTS `tb_endereco`;
+CREATE TABLE IF NOT EXISTS `tb_endereco` (
+  `idEndereco` varchar(50) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `uf` tinytext NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `rua` varchar(50) NOT NULL,
+  `cep` int(11) NOT NULL,
+  `numero` smallint(6) NOT NULL,
+  `complemento` varchar(100) NOT NULL,
+  PRIMARY KEY (`idEndereco`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+CREATE TABLE IF NOT EXISTS `tb_usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `idEndereco` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `idContato` int(11) NOT NULL,
+  `nome` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `cpf` bigint(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(40) NOT NULL,
+  PRIMARY KEY (`idUsuario`),
+  KEY `idContato_fk` (`idContato`),
+  KEY `idEndereco_fk` (`idEndereco`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `tb_usuario`
+--
+ALTER TABLE `tb_usuario`
+  ADD CONSTRAINT `idContato_fk` FOREIGN KEY (`idContato`) REFERENCES `tb_contato` (`idContato`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `idEndereco_fk` FOREIGN KEY (`idEndereco`) REFERENCES `tb_endereco` (`idEndereco`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
