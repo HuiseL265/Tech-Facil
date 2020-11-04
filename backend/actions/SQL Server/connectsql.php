1d6db6bc00fbf1fba265527ea472e64b45af6dcb
unknown <vitor.per55@gmail.com>
2020-11-04 12:01:20 -0300

    Banco hospedado e alocado as informações de conexão

---
+++ backend/actions/SQL Server/connectsql.php
@@ -0,0 +1,15 @@
+<?php
+
+$serverName = "SQL5097.site4now.net";
+$connectionInfo = array( "Database"=>"DB_A69E92_dbTechFacil", "UID"=>"DB_A69E92_dbTechFacil_admin", "PWD"=>"Techfacil520");
+
+$con = sqlsrv_connect( $serverName, $connectionInfo);
+
+if($con) {
+    echo "<p style=display:none;>Connection established.</p>";
+}else{
+    echo "Connection could not be established.<br />";
+    die( print_r( sqlsrv_errors(), true));
+}
+
+?>
