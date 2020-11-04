49041bc66f4670fdd5205db1e088815eed3b9cfd
unknown <vitor.per55@gmail.com>
2020-11-04 20:54:58 -0300

    info. adicional nos comandos

--- backend/actions/SQL Server/comandosTechFacil.sql
+++ backend/actions/SQL Server/comandosTechFacil.sql
@@ -14,7 +14,7 @@ Tb_PrestadorDeServico.idPrestador as ID_Prestador,
 Tb_Usuario.Nome,
 Tb_Usuario.CPF,
 Tb_Usuario.Email,
-Tb_Usuario.dataCriacao as DataDeCriação
+Tb_Usuario.dataCriacao as DataDeCriaï¿½ï¿½o
 FROM Tb_PrestadorDeServico
 LEFT JOIN Tb_Usuario
 ON Tb_Usuario.idUsuario = Tb_PrestadorDeServico.idUsuario
@@ -25,12 +25,12 @@ Tb_Contratante.idContratante as ID_Contratante,
 Tb_Usuario.Nome,
 Tb_Usuario.CPF,
 Tb_Usuario.Email,
-Tb_Usuario.dataCriacao as DataDeCriação
+Tb_Usuario.dataCriacao as DataDeCriaï¿½ï¿½o
 FROM Tb_Contratante
 LEFT JOIN Tb_Usuario
 ON Tb_Usuario.idUsuario = Tb_Contratante.idUsuario
 
----Verificar Endereço por Usuario
+---Verificar Endereï¿½o por Usuario
 SELECT 
 Tb_Usuario.idUsuario as ID_Usuario,
 Tb_Usuario.Nome,
@@ -49,7 +49,7 @@ ON Tb_Usuario.idUsuario = Tb_Endereco.idUsuario
 SELECT 
 Tb_Usuario.idUsuario as ID_Usuario,
 Tb_Usuario.Nome,
-Tb_Contato.EmailSecundario as Email_Secundário,
+Tb_Contato.EmailSecundario as Email_Secundï¿½rio,
 Tb_Contato.Telefone1,
 Tb_Contato.Telefone2
 FROM Tb_Contato
@@ -70,9 +70,9 @@ ON Tb_Contratante.idContratante = Tb_RequisicaoProblema.idContratante
 LEFT JOIN Tb_Usuario
 ON Tb_Usuario.idUsuario = Tb_Contratante.idUsuario
 
----Verificar Verificação
+---Verificar Verificaï¿½ï¿½o
 SELECT 
-Tb_Verificacao.idVerificacao as ID_Verificação,
+Tb_Verificacao.idVerificacao as ID_Verificaï¿½ï¿½o,
 Tb_PrestadorDeServico.idPrestador as ID_Contratante,
 Tb_Usuario.Nome,
 Tb_Verificacao.Status,
@@ -114,10 +114,10 @@ usuario.Nome,
 usuario.CPF,
 usuario.tipoUsuario,
 escolaridade.curso as Curso,
-escolaridade.instituicao as Instituição,
-escolaridade.nivelFormacao as Formação,
-escolaridade.situacao as Situação,
-escolaridade.conclusaoData as Data_de_Conclusão
+escolaridade.instituicao as Instituiï¿½ï¿½o,
+escolaridade.nivelFormacao as Formaï¿½ï¿½o,
+escolaridade.situacao as Situaï¿½ï¿½o,
+escolaridade.conclusaoData as Data_de_Conclusï¿½o
 FROM Tb_Escolaridade as escolaridade
 
 INNER JOIN Tb_Curriculo
@@ -136,10 +136,10 @@ SELECT
 usuario.Nome,
 usuario.CPF,
 usuario.tipoUsuario,
-curso.nivelFormacao as Formação,
+curso.nivelFormacao as Formaï¿½ï¿½o,
 curso.curso as Curso,
-curso.instituicao as Instituição,
-curso.conclusaoData as Data_de_Conclusão
+curso.instituicao as Instituiï¿½ï¿½o,
+curso.conclusaoData as Data_de_Conclusï¿½o
 FROM Tb_Cursos as curso
 
 INNER JOIN Tb_Curriculo
@@ -158,7 +158,7 @@ usuario.Nome,
 usuario.CPF,
 usuario.tipoUsuario,
 experiencia.nomeEmpresa as Empresa,
-experiencia.funcao as Função,
+experiencia.funcao as Funï¿½ï¿½o,
 experiencia.atividades,
 experiencia.dataEntrada,
 experiencia.dataSaida
@@ -174,7 +174,7 @@ LEFT JOIN Tb_Usuario as usuario
 ON usuario.idUsuario = Tb_PrestadorDeServico.idUsuario
 
 
----Exemplo de inserção de novo prestador
+---Exemplo de inserï¿½ï¿½o de novo prestador
 INSERT INTO Tb_Usuario (idUsuario, Nome, CPF, Email, Senha, dataCriacao)
 VALUES(3, 'Guilherme Ferreira', 18222222991, 'galin@gmail.com', '29515bb9a5d5e558e2b3ba71e3b6e037', GETDATE())
 
@@ -182,50 +182,62 @@ VALUES(3, 'Guilherme Ferreira', 18222222991, 'galin@gmail.com', '29515bb9a5d5e55
 INSERT INTO Tb_PrestadorDeServico(idUsuario,idPrestador,idVerificacao)
 VALUES(3,2,NULL)
 
----Exemplo de inserção de novo contrante
+---Exemplo de inserï¿½ï¿½o de novo contrante
 INSERT INTO Tb_Usuario (idUsuario, Nome, CPF, Email, Senha, dataCriacao)
 VALUES(2, 'Thiago Frederico', 18309888821, 'thiagofred@gmail.com', '29515bb9a5d5e558e2b3ba71e3b6e037', GETDATE())
 
 INSERT INTO Tb_Contratante(idUsuario,idContratante)
 VALUES(2,1,NULL,NULL)
 
---Exemplo de inserção de endereço
+--Exemplo de inserï¿½ï¿½o de endereï¿½o
 INSERT INTO Tb_Endereco (idUsuario,Rua,Cidade,Bairro,CEP,UF,Numero,Complemento)
-VALUES(1,'Rua Edson Danillo Dotto','São Judas','Lapa',08384370,'SP',157,'Perto do corpo de bombeiros')
+VALUES(1,'Rua Edson Danillo Dotto','Sï¿½o Judas','Lapa',08384370,'SP',157,'Perto do corpo de bombeiros')
 
---Exemplo de inserção de contato
+--Exemplo de inserï¿½ï¿½o de contato
 INSERT INTO Tb_Contato (idUSuario,Telefone1,Telefone2,EmailSecundario)
 VALUES (1,11982547083, NULL, 'vitor-per@hotmail.com')
 
---Exemplo de criação de Requisicao
+--Exemplo de criaï¿½ï¿½o de Requisicao
 INSERT INTO Tb_RequisicaoProblema (idContratante,idRequisicao,Status,tipoProblema,Contexto)
-VALUES (1,1,'Aberto', 'Erro Hardware', 'Meu computador não consegue iniciar. Aparace um problema com o hardware fez com que o Windows parasse de funcionar corretamente.')
+VALUES (1,1,'Aberto', 'Erro Hardware', 'Meu computador nï¿½o consegue iniciar. Aparace um problema com o hardware fez com que o Windows parasse de funcionar corretamente.')
 
---Exemplo de criação do Acompanhamento
+--Exemplo de criaï¿½ï¿½o do Acompanhamento
 INSERT INTO Tb_Acompanhamento(idRequisicao,idContratante,idPrestador,dataAbertura,dataFechamento)
 VALUES (1,1,1,GETDATE(),NULL)
 
---Exemplo de criação da Verificação para o Prestador 
+--Exemplo de criaï¿½ï¿½o da Verificaï¿½ï¿½o para o Prestador 
+--OBS:Ao incluir a verificaÃ§Ã£o para o prestador, Ã© necessÃ¡rio colocar
+--o idVerificaÃ§Ã£o tambÃ©m na tabela Prestador
 INSERT INTO Tb_Verificacao(idVerificacao,Status,diretorioArquivo,nomeArquivo,tipoArquivo)
 VALUES (1,'Pendente','C:\Program Files (x86)','Foto','.jpeg');
 
+INSERT INTO Tb_PrestadorDeServico(idPrestador,idVerificacao)
+VALUES (1,1);
 
---Exemplo de criação da Verificação para o Prestador 
+--Exemplo de criaï¿½ï¿½o da Verificaï¿½ï¿½o para o Prestador 
 INSERT INTO Tb_Verificacao(idVerificacao,Status,diretorioArquivo,nomeArquivo,tipoArquivo)
 VALUES (2,'Pendente','C:\Users\Public\Pictures','MinhaFoto','.png');
 
---Exemplo de criação do Curriculo do Prestador
+--Exemplo de criaï¿½ï¿½o do Curriculo do Prestador
+--OBS:Ao incluir um curriculo para o prestador, Ã© necessÃ¡rio colocar
+--o idCurriculo tambÃ©m na tabela Prestador
+
 INSERT INTO Tb_Curriculo (idCurriculo,idPrestador)
 VALUES(2,2)
 
+--Linkando o Curriculo com prestador
+
+INSERT INTO Tb_PrestadorDeServico(idPrestador,idCurriculo)
+VALUES(2,2)
+
 INSERT INTO Tb_Escolaridade (idCurriculo,nivelFormacao,instituicao,curso,situacao,conclusaoData)
-VALUES(2,'Ensino Médio Técnico','Etec Cidade Tiradentes','Desenvolvimento de Sistemas','Cursando',NULL)
+VALUES(2,'Ensino Mï¿½dio Tï¿½cnico','Etec Cidade Tiradentes','Desenvolvimento de Sistemas','Cursando',NULL)
 
 INSERT INTO Tb_Cursos(idCurriculo,nivelFormacao,instituicao,curso,conclusaoData,codCertificado)
 VALUES(2,'EAD','Microsoft Academy','Machine Learning', GETDATE(),NULL)
 
 INSERT INTO Tb_Experiencia(idCurriculo,nomeEmpresa,funcao,atividades,dataEntrada,dataSaida)
-VALUES(2,'Sorveteria','Analista de Sistemas','Analisar programação de sorveteria',GETDATE(),NULL)
+VALUES(2,'Sorveteria','Analista de Sistemas','Analisar programaï¿½ï¿½o de sorveteria',GETDATE(),NULL)
 
 INSERT INTO Tb_Habilidades (idCurriculo,habilidade)
-VALUES(2,'Pular do helicóptero')
+VALUES(2,'Pular do helicï¿½ptero')
